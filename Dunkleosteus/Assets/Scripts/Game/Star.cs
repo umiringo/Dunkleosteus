938e922f1d8bfafa8b8d2d3ddfbe4bf6e96c7943
umiringo <umiringo42@gmail.com>
2016-05-30 00:25:45 +0800

    修改altas

--- Dunkleosteus/Assets/Scripts/Game/Star.cs
+++ Dunkleosteus/Assets/Scripts/Game/Star.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GlobalDefines;
 
 public class Star : MonoBehaviour {
 
+    public GameObject starChild;
+    public GameObject shineChild;
+    public int index;
+    public int state;
 	// Use this for initialization
 	void Start () {
-	
+        index = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void SetStatus(int s)
+    {
+        state = s;
+        // TODO
+        // 修改child的sprite
+    }
 }
