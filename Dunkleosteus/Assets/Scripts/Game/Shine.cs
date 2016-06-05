3e925dd29f97968eed8463f55d1b0155c551c2b3
umiringo <umiringo42@gmail.com>
2016-06-05 10:11:09 +0800

    add some function

--- Dunkleosteus/Assets/Scripts/Game/Shine.cs
+++ Dunkleosteus/Assets/Scripts/Game/Shine.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class Shine : MonoBehaviour {
-
+    private int shineIntervalMin;
+    private int shineIntervalMax;
 	// Use this for initialization
 	void Start () {
-	
+        shineIntervalMin = 5;
+        shineIntervalMax = 20;
+	    
+        //开启协程
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    IEnumerator DoShine()
+    {
+        int interval = Random.Range(shineIntervalMin, shineIntervalMax);
+        yield return new WaitForSeconds(interval);
+        //tween 启动
+    }
 }
