0a06710cd772d92a05d374475d2325dc1138ab25
umiringo <umiringo42@gmail.com>
2016-10-31 11:46:26 +0800

    add all persus cons

--- Dunkleosteus/Assets/Scripts/Game/Model/GameDirector.cs
+++ Dunkleosteus/Assets/Scripts/Game/Model/GameDirector.cs
@@ -81,7 +81,7 @@ public class GameDirector : MonoBehaviour {
     {
         PlayerPrefs.DeleteAll();
         // Init latestLevel
-        string latestLevel = PlayerPrefs.GetString(PlayerPrefsKey.LatestLevel, "Eridanus");
+        string latestLevel = PlayerPrefs.GetString(PlayerPrefsKey.LatestLevel, "Norma");
         // first level
         if(latestLevel == "begin") {
             PlayerPrefs.SetString(PlayerPrefsKey.LatestLevel, latestLevel);
