b35b50da5539aa2493eaf293e2287d73e941014f
umiringo <umiringo42@gmail.com>
2016-11-01 15:03:49 +0800

    add pavo,grus,chame,dorado

--- Dunkleosteus/Assets/Scripts/Game/Model/GameDirector.cs
+++ Dunkleosteus/Assets/Scripts/Game/Model/GameDirector.cs
@@ -81,7 +81,7 @@ public class GameDirector : MonoBehaviour {
     {
         PlayerPrefs.DeleteAll();
         // Init latestLevel
-        string latestLevel = PlayerPrefs.GetString(PlayerPrefsKey.LatestLevel, "Hydra");
+        string latestLevel = PlayerPrefs.GetString(PlayerPrefsKey.LatestLevel, "Grus");
         // first level
         if(latestLevel == "begin") {
             PlayerPrefs.SetString(PlayerPrefsKey.LatestLevel, latestLevel);
