3e94bb07a4ec6a55d77dbc2b0c9f461fed78f05c
umiringo <umiringo42@gmail.com>
2017-03-24 12:07:26 +0800

    cx painter

--- Dunkleosteus/Assets/Scripts/Game/Model/GameDirector.cs
+++ Dunkleosteus/Assets/Scripts/Game/Model/GameDirector.cs
@@ -124,7 +124,7 @@ public class GameDirector : MonoBehaviour {
 
     private void LoadPlayerPrefs()
     {
-        // PlayerPrefs.DeleteAll();
+        //PlayerPrefs.DeleteAll();
         // Init latestLevel
         string latestLevel = PlayerPrefs.GetString(PlayerPrefsKey.LatestLevel, "begin");
         // first level
